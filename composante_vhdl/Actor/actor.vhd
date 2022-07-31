28aa87944fad5437a9be9b272567bd391666a188
veronique <veronique.paquin2@usherbrooke.ca>
2022-07-31 18:27:27 -0400

    TB actor fonctionnel

--- composante_vhdl/Actor/actor.vhd
+++ composante_vhdl/Actor/actor.vhd
@@ -1,3 +1,4 @@
+
 ----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
@@ -42,72 +43,56 @@ entity actor is
            i_curr_actor_id  : in STD_LOGIC_VECTOR (3 downto 0);
            i_we_pos         : in STD_LOGIC;
            i_we_tile        : in STD_LOGIC;
-           i_we_actif       : in STD_LOGIC;
+            i_we_actif       : in STD_LOGIC;
            o_tid            : out STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
            o_pix_x          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
            o_pix_y          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
-           o_actif          : out STD_LOGIC;
+             o_actif          : out STD_LOGIC;
            o_hit            : out STD_LOGIC := '0'); -- quand a 1, l'acteur se trouve a la pos globale
 end actor;
 
 architecture Behavioral of actor is   
-
-    signal s_tid        : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
-    signal s_actif      : STD_LOGIC := '0';
-    
-    signal s_pos_x  : INTEGER := 0;
-    signal test     : INTEGER := 0;
-    signal test2     : INTEGER := 0;
-    signal s_pos_y  : INTEGER := 0;
-    signal s_gb_x   : INTEGER := 0;
-    signal s_gb_y   : INTEGER := 0;
-    signal test3          : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
+signal s_actif      : STD_LOGIC := '0';
+    signal s_pos_x  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
+    signal s_pos_y  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
+    signal test2  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
+    signal test     : INTEGER;
+    signal s_tid    : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
 
 begin
-    s_gb_x  <= TO_INTEGER(UNSIGNED(i_gb_x));
-    s_gb_y  <= TO_INTEGER(UNSIGNED(i_gb_y));
-    o_tid   <= s_tid;
-    o_actif <= s_actif;
-    
-    -- write-enable set position
-    process(i_we_pos)
+    process(i_curr_actor_id, i_gb_x, i_gb_y)
     begin
-        if(i_we_pos = '1' and i_curr_actor_id = i_actor_id) then
-            s_pos_x <= TO_INTEGER(UNSIGNED(i_set_x));
-            s_pos_y <= TO_INTEGER(UNSIGNED(i_set_y));
+        if(i_curr_actor_id = i_actor_id) then
+            if(i_we_pos = '1') then
+                s_pos_x <= i_set_x;
+                s_pos_y <= i_set_y;
+            end if;
+            
+            if(i_we_tile = '1') then
+                s_tid <= i_tid;
+            end if;
+            
+            -- Comparateur
+            if(((s_pos_x <= i_gb_x) and (to_integer(unsigned(s_pos_x)) + 16 > to_integer(unsigned(i_gb_x)))) and
+              (((s_pos_y <= i_gb_y) and (to_integer(unsigned(s_pos_y)) + 16 > to_integer(unsigned(i_gb_y)))))) then 
+                o_hit <= '1';
+               -- test2 <= std_logic_vector((unsigned(i_gb_x) + unsigned(s_pos_x)));
+                test <= to_integer(unsigned(i_gb_x)) - to_integer(unsigned(s_pos_x));
+                
+                o_pix_x <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_x)) - to_integer(unsigned(s_pos_x)),o_pix_x'length ));
+                o_pix_y <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_y)) - to_integer(unsigned(s_pos_y)),o_pix_y'length ));
+            else
+                o_hit <= '0';
+            end if;
+
         end if;
     end process;
-    
-    -- write-enable set tile id
-    process(i_we_tile)
-    begin
-        if(i_we_tile = '1' and i_curr_actor_id = i_actor_id) then
-            s_tid <= i_tid;
-        end if;
-    end process;
-    
     
     -- write-enable set actor id
     process(i_we_actif)
     begin
         s_actif <= i_we_actif;
     end process;
-    
-    -- Comparateur
-    process(i_gb_x, i_gb_y)
-    begin
-        if(((s_pos_x <= s_gb_x) and (s_pos_x + 16 > s_gb_x)) and
-          ((s_pos_y <= s_gb_y) and (s_pos_y + 16 > s_gb_y))) then 
-            o_hit <= '1';                
-            test3 <= std_logic_vector(TO_UNSIGNED(2-0,test3'length));       -- 2 - 0 = 2
-            test <= 2 - 0;                                                  -- 2 - 0 = 2
-            test2 <= (s_gb_x) - (s_pos_x);                                  -- 2 - 0 = 1
-            o_pix_x <= std_logic_vector(TO_UNSIGNED((s_gb_x) - (s_pos_x), o_pix_x'length));
-            o_pix_y <= std_logic_vector(TO_UNSIGNED((s_gb_y) - (s_pos_y), o_pix_y'length));
-        else
-            o_hit <= '0';
-        end if;
+    o_tid <= s_tid;
         
-    end process;
-   
 end Behavioral;
--- composante_vhdl/Actor/actor_tb.vhd
+++ composante_vhdl/Actor/actor_tb.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 07/28/2022 03:30:36 PM
+-- Create Date: 31.07.2022 11:31:47
 -- Design Name: 
--- Module Name: actor_tb - Behavioral
+-- Module Name: Actor_TB - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,25 +21,24 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity actor_tb is
+entity Actor_TB is
 --  Port ( );
-end actor_tb;
+end Actor_TB;
 
-architecture Behavioral of actor_tb is
+architecture Behavioral of Actor_TB is
 
     component actor is
-    Port ( i_gb_x           : in STD_LOGIC_VECTOR (8 downto 0);
+          Port ( i_gb_x           : in STD_LOGIC_VECTOR (8 downto 0);
            i_gb_y           : in STD_LOGIC_VECTOR (8 downto 0);
            i_set_x          : in STD_LOGIC_VECTOR (8 downto 0);
            i_set_y          : in STD_LOGIC_VECTOR (8 downto 0);
@@ -48,22 +47,21 @@ architecture Behavioral of actor_tb is
            i_curr_actor_id  : in STD_LOGIC_VECTOR (3 downto 0);
            i_we_pos         : in STD_LOGIC;
            i_we_tile        : in STD_LOGIC;
-           i_we_actif       : in STD_LOGIC;
+            i_we_actif       : in STD_LOGIC;
            o_tid            : out STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
            o_pix_x          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
            o_pix_y          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
-           o_actif          : out STD_LOGIC;
-           o_hit            : out STD_LOGIC := '0'); -- quand a 1, l'acteur se trouve a la pos globale
+             o_actif          : out STD_LOGIC;
+           o_hit            : out STD_LOGIC := '0');  -- quand a 1, l'acteur se trouve a la pos globale
     end component;
     
-    -- signaux en sortie
+    -- signaux de sortie
     signal sim_tid          : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
     signal sim_pix_x        : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
     signal sim_pix_y        : STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
     signal sim_hit          : STD_LOGIC := '0'; 
-    signal sim_actif          : STD_LOGIC := '0'; 
     
-    -- signaux en entree
+    -- signaux d'entree
     signal test_gb_x              : INTEGER;
     signal test_gb_y              : INTEGER;
     signal test_gb_x_b            : STD_LOGIC_VECTOR (8 downto 0);
@@ -75,7 +73,7 @@ architecture Behavioral of actor_tb is
     signal test_curr_actor_id     : STD_LOGIC_VECTOR (3 downto 0);
     signal test_we_pos            : STD_LOGIC;
     signal test_we_tile           : STD_LOGIC;
-    signal test_we_actif           : STD_LOGIC;
+    signal sim_actif : std_logic;
     
     CONSTANT PERIOD    : time := 10 ns;
     
@@ -88,45 +86,42 @@ begin
            i_set_x          => test_set_x, 
            i_set_y          => test_set_y,        
            i_tid            => test_tid,          
-           i_actor_id       => test_actor_id,     
-           i_curr_actor_id  => test_curr_actor_id,
-           i_we_pos         => test_we_pos,       
-           i_we_tile        => test_we_tile,      
-           i_we_actif       => test_we_actif,      
+           i_actor_id       => x"0",     
+           i_curr_actor_id  => x"0",
+           i_we_pos         => '1',       
+           i_we_tile        => '1',   
+           i_we_actif       => '1',   
            o_tid            => sim_tid,  
            o_pix_x          => sim_pix_x,
            o_pix_y          => sim_pix_y,
-           o_hit            => sim_hit,  
-           o_actif          => sim_actif
+           o_actif          => sim_actif,
+           o_hit            => sim_hit  
     );
     
     
-   tb : PROCESS
+    tb : PROCESS
    BEGIN
      
      wait for PERIOD;  
      test_set_x         <= "000000000";
      test_set_y         <= "000000000";
-     test_we_pos        <= '1';
-     test_we_tile       <= '1';
-     test_tid           <= "00000001";
-     test_actor_id      <= "0000";
-     test_curr_actor_id <= "0000";
-     
-     wait for PERIOD;  
-     test_set_x         <= "000000001";
-     test_set_y         <= "000000000";
-     test_we_pos        <= '0';
-     test_we_tile       <= '0';
      test_tid           <= "00000000";
-     test_actor_id      <= "0000";
-      
+     wait for PERIOD;  wait for PERIOD;  wait for PERIOD;  wait for PERIOD;  
+--     wait for PERIOD;  
+--     test_set_x         <= "000000000";
+--     test_set_y         <= "000000001";
+--     test_we_pos        <= '0';
+--     test_we_tile       <= '0';
+--     test_tid           <= "00000000";
+--     test_actor_id      <= "0000";
+      test_set_x         <= "000000010";
+     test_set_y         <= "000001000";
       WAIT; -- will wait forever
    END PROCESS;
    
     
-    test_gb_x_b <= std_logic_vector(TO_UNSIGNED(test_gb_x, test_gb_x_b'length));
-    test_gb_y_b <= std_logic_vector(TO_UNSIGNED(test_gb_y, test_gb_y_b'length));
+    test_gb_x_b <= std_logic_vector(TO_UNSIGNED(test_gb_x, test_gb_x_b'length ));
+    test_gb_y_b <= std_logic_vector(TO_UNSIGNED(test_gb_y, test_gb_y_b'length ));
     
  
    
