f412c08eb5fdfff60e2000165a2c7118f774bc66
Jacty Saenz <saej3101@usherbrooke.ca>
2022-08-01 17:46:03 -0400

    Correction color converter

--- composante_vhdl/Actor/actor.vhd
+++ composante_vhdl/Actor/actor.vhd
@@ -1,4 +1,3 @@
-
 ----------------------------------------------------------------------------------
 -- Company: 
 -- Engineer: 
@@ -43,56 +42,67 @@ entity actor is
            i_curr_actor_id  : in STD_LOGIC_VECTOR (3 downto 0);
            i_we_pos         : in STD_LOGIC;
            i_we_tile        : in STD_LOGIC;
-            i_we_actif       : in STD_LOGIC;
+           i_we_actif       : in STD_LOGIC;
            o_tid            : out STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
            o_pix_x          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
            o_pix_y          : out STD_LOGIC_VECTOR (3 downto 0) := (others => '0');
-             o_actif          : out STD_LOGIC;
+           o_actif          : out STD_LOGIC;
            o_hit            : out STD_LOGIC := '0'); -- quand a 1, l'acteur se trouve a la pos globale
 end actor;
 
 architecture Behavioral of actor is   
-signal s_actif      : STD_LOGIC := '0';
+
+    signal s_tid        : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+    signal s_actif      : STD_LOGIC := '0';
+    
     signal s_pos_x  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
     signal s_pos_y  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
-    signal test2  : STD_LOGIC_VECTOR (8 downto 0) := (others => '0');
-    signal test     : INTEGER;
-    signal s_tid    : STD_LOGIC_VECTOR (7 downto 0) := (others => '0');
+--    signal s_gb_x   : INTEGER := 0;
+--    signal s_gb_y   : INTEGER := 0;
+    
 
 begin
-    process(i_curr_actor_id, i_gb_x, i_gb_y)
+--    s_gb_x  <= TO_INTEGER(UNSIGNED(i_gb_x));
+--    s_gb_y  <= TO_INTEGER(UNSIGNED(i_gb_y));
+    o_tid   <= s_tid;
+    o_actif <= s_actif;
+    
+    -- write-enable set position
+    process(i_we_pos)
     begin
-        if(i_curr_actor_id = i_actor_id) then
-            if(i_we_pos = '1') then
-                s_pos_x <= i_set_x;
-                s_pos_y <= i_set_y;
-            end if;
-            
-            if(i_we_tile = '1') then
-                s_tid <= i_tid;
-            end if;
-            
-            -- Comparateur
-            if(((s_pos_x <= i_gb_x) and (to_integer(unsigned(s_pos_x)) + 16 > to_integer(unsigned(i_gb_x)))) and
-              (((s_pos_y <= i_gb_y) and (to_integer(unsigned(s_pos_y)) + 16 > to_integer(unsigned(i_gb_y)))))) then 
-                o_hit <= '1';
-               -- test2 <= std_logic_vector((unsigned(i_gb_x) + unsigned(s_pos_x)));
-                test <= to_integer(unsigned(i_gb_x)) - to_integer(unsigned(s_pos_x));
-                
-                o_pix_x <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_x)) - to_integer(unsigned(s_pos_x)),o_pix_x'length ));
-                o_pix_y <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_y)) - to_integer(unsigned(s_pos_y)),o_pix_y'length ));
-            else
-                o_hit <= '0';
-            end if;
-
+        if(i_we_pos = '1' and i_curr_actor_id = i_actor_id) then
+            s_pos_x <= i_set_x;
+            s_pos_y <= i_set_y;
         end if;
     end process;
+    
+    -- write-enable set tile id
+    process(i_we_tile)
+    begin
+        if(i_we_tile = '1' and i_curr_actor_id = i_actor_id) then
+            s_tid <= i_tid;
+        end if;
+    end process;
+    
     
     -- write-enable set actor id
     process(i_we_actif)
     begin
         s_actif <= i_we_actif;
     end process;
-    o_tid <= s_tid;
+    
+    -- Comparateur
+    process(i_gb_x, i_gb_y)
+    begin
+        if(((s_pos_x <= i_gb_x) and (to_integer(unsigned(s_pos_x)) + 16 > to_integer(unsigned(i_gb_x)))) and
+          (((s_pos_y <= i_gb_y) and (to_integer(unsigned(s_pos_y)) + 16 > to_integer(unsigned(i_gb_y)))))) then 
+            o_hit <= '1';
+            o_pix_x <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_x)) - to_integer(unsigned(s_pos_x)),o_pix_x'length ));
+            o_pix_y <= std_logic_vector(to_unsigned(to_integer(unsigned(i_gb_y)) - to_integer(unsigned(s_pos_y)),o_pix_y'length ));
+        else
+            o_hit <= '0';
+        end if;
         
+    end process;
+   
 end Behavioral;
--- composante_vhdl/color_converter.vhd
+++ composante_vhdl/color_converter.vhd
@@ -2,29 +2,29 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity color_converter is
-    Port ( code : in STD_LOGIC_VECTOR (3 downto 0);
-           color : out STD_LOGIC_VECTOR (23 downto 0));
+    Port ( code     : in STD_LOGIC_VECTOR (3 downto 0);
+           color    : out STD_LOGIC_VECTOR (23 downto 0));
 end color_converter;
 
 architecture Behavioral of color_converter is
 
 begin
     color <= 
-            x"000000" when code = x"0"  else -- noire
-            x"FFFFFF" when code = x"1"  else -- blanc
-            x"00A2E8" when code = x"2"  else -- bleu pale
-            x"ED1A2C" when code = x"3"  else -- rouge
-            x"C85086" when code = x"4"  else -- rose
-            x"F7AB79" when code = x"5"  else -- beige
-            x"B5E61D" when code = x"6"  else -- lime
-            x"0c6624" when code = x"7"  else -- vert foret
-            x"22b14c" when code = x"8"  else -- vert buisson
-            x"8A5132" when code = x"9"  else -- brun
-            x"FF7F2F" when code = x"A" else -- orange
-            x"FFF200" when code = x"B" else -- jaune
-            x"66696B" when code = x"C" else -- gris 
-            x"000000" when code = x"D" else -- undefined
-            x"000000" when code = x"E" else -- undefined
-            x"000000" when code = x"F" else -- undefined
-            x"000000"; -- undefined
+            x"C85087" when code = x"0"  else    
+            x"000000" when code = x"1"  else    -- noire              
+            x"FFFFFF" when code = x"2"  else    -- blanc              
+            x"00A2E8" when code = x"3"  else    -- bleu pale  
+            x"ED1A2C" when code = x"4"  else    -- rouge       
+            x"C85086" when code = x"5"  else    -- rose              
+            x"F7AB79" when code = x"6"  else    -- beige               
+            x"B5E61D" when code = x"7"  else    -- lime            
+            x"0c6624" when code = x"8"  else    -- vert foret   
+            x"22b14c" when code = x"0"  else    -- vert buisson 
+            x"8A5132" when code = x"A"  else    -- brun         
+            x"FF7F2F" when code = x"B" else     -- orange       
+            x"FFF200" when code = x"C" else     -- jaune        
+            x"7a767a" when code = x"D" else     -- gris         
+            x"2121B8" when code = x"E" else     -- bleu fonce    
+            x"C85087" when code = x"F" else     -- undefined    
+            x"C85087";    
 end Behavioral;
